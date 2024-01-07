1e7a6179a253004d16ab283daa836e8d2d39f5b1
kouhei-github <kohei0801nagamatsu@gmail.com>
2024-01-07 22:31:13 +0900

    バックエンドからAWS Lambda固有の設定を削除する
    このコミットでは、バックエンドサーバーのコードから AWS Lambda 固有の設定をすべて削除しました。AWS Lambda 関数として実行する代わりに、サーバーは常にポート 80 で起動するようになりました。Lambda 固有のパッケージと設定は削除され、コードはシンプルになりました。DockerfileもLambdaのベースイメージから標準のGolangイメージに更新されました。

--- src/main.go
+++ src/main.go
@@ -1,17 +1,10 @@
 package main
 
 import (
-	"context"
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
-	fiberadapter "github.com/awslabs/aws-lambda-go-api-proxy/fiber"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/kouhei-github/ai-interview-golang/route"
-	"os"
 )
-
-var fiberLambda *fiberadapter.FiberLambda
 
 const MaxBodySize = 500 * 1024 * 1024
 
@@ -40,21 +33,7 @@ func main() {
 	route.LoadRouter(router)
 
 	// Webサーバー起動時のエラーハンドリング => localhostの時コメントイン必要
-	if os.Getenv("ENVIRONMENT") == "local" {
-		if err := router.FiberApp.Listen(":80"); err != nil {
-			panic(err)
-		}
-	} else {
-		// fmt.Println("lambda")
-		// AWS Lambdaとの連携設定
-		fiberLambda = fiberadapter.New(router.FiberApp)
-		lambda.Start(Handler)
+	if err := router.FiberApp.Listen(":80"); err != nil {
+		panic(err)
 	}
-}
-
-// Handler is a function that handles HTTP requests and returns an HTTP response.
-// It uses the FiberLambda proxy with context method to pass control to the Lambda function.
-func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	// If no name is provided in the HTTP request body, throw an error
-	return fiberLambda.ProxyWithContext(ctx, req)
 }
